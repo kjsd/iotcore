39eb2d59c99c0b5f30cee246ad1288d7bc119a96
Kenji MINOURA <info@kandj.org>
2018-07-18 20:16:47 +0900

    sigfox downlink

--- app.js
+++ app.js
@@ -13,8 +13,52 @@
  ***********************************************************************/
 'use strict';
 
-console.log('hello world');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-var server = app.listen(process.env.PORT || '3000', function () {
+if (process.env.NODE_ENV == 'production') {
+  app.use(require('compression')());
+  app.use(require('express-minify')());
+} else {
+  process.env.BASE_URL = 'http://localhost:3000';
+}
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+const toDownlinkData = function(v) {
+  let s = v.toString(16).split('').reverse().join('');
+  if (s.length > 16) return false;
+
+  for (let i = s.length; i < 16; i++) {
+    s += '0';
+  }
+
+  return s;
+};
+
+app.post('/:device', (req, res) => {
+  console.log(req.params.device);
+  console.log(req.body);
+
+  if (!req.body.ack) {
+    res.sendStatus(204);
+    return;
+  }
+
+  const uploadSec = 3600; // 1hour
+
+  let downlink = new Object();
+  downlink[req.params.device] = {
+    'downlinkData': toDownlinkData(uploadSec)
+  };
+
+  res.json(downlink);
+});
+
+const server = app.listen(process.env.PORT || '3000', function () {
   console.log('App listening on port %s', server.address().port);
 });
