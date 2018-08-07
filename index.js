306c61a79e6f0fe389ca286c4e0b2ec325894550
Kenji MINOURA <info@kandj.org>
2018-08-07 14:54:44 +0900

    info/log

--- index.js
+++ index.js
@@ -26,60 +26,115 @@ function toDownlinkData(v) {
   return prefix + s;
 }
 
-function getDevices() {
-}
-
-function getDevice(id) {
+function getInfo(id) {
   const key = datastore.key(['DeviceInfo', id]);
 
   return datastore.get(key);
 }
 
-exports.devices = (req, res) => {
+function saveInfo(id, data) {
+  const key = datastore.key(['DeviceInfo', id]);
+
+  return datastore.save({
+    key: key,
+    data: {
+      name: data.name,
+      interval: data.interval
+    }
+  });
+}
+
+function getLog(id) {
+  const key = datastore.key(['DeviceLog', id]);
+
+  return datastore.get(key);
+}
+
+function saveLog(id, data) {
+  const keyInfo = datastore.key(['DeviceInfo', id]);
+  const keyLog = datastore.key(['DeviceLog', id]);
+  const transaction = datastore.transaction();
+
+  const saveLogImpl = () => {
+    return transaction.save({
+      key: keyLog,
+      data: {
+        time: data.time,
+        data: data.data,
+        lat: data.lat,
+        lng: data.lng
+      }
+    });
+  };
+
+  return transaction.run()
+    .then(() => transaction.get(keyInfo))
+    .then((err, entity) => {
+      if (err) {
+        transaction.save({
+          key: keyInfo,
+          data: {
+            name: '__DEFAULT__',
+            interval: 3600
+          }
+        });
+      }
+
+      transaction.save({
+        key: keyLog,
+        data: {
+          time: data.time,
+          data: data.data,
+          lat: data.lat,
+          lng: data.lng
+        }
+      });
+
+      return transaction.commit();
+    }).then(() => {
+      return getInfo(id);
+    }).catch(() => transaction.rollback());
+}
+
+exports.log = (req, res) => {
   console.log(req.params[0]);
   console.log(req.body);
 
   const id = req.params[0].substr(1);
+  if (!id) {
+    res.sendStatus(400);
+    return;
+  }
 
   switch (req.method) {
   case 'GET':
-    const data = (id ? getDevice(id): getDevices());
-    if (!data) {
-      res.sendStatus(404);
-      return;
-    }
-
-    res.json(data);
+    getLog(id).then(data => res.json(data)).catch(() => res.sendStatus(404));
     return;
 
   case 'POST':
-    if (!id) {
-      res.sendStatus(400);
+    const promise = saveLog(id, req.body);
+
+    if (!req.body.ack) {
+      res.sendStatus(204);
       return;
     }
 
-    let saveAndReturn = (interval) => {
-      saveDevice(device, req.body);
-
-      if (!req.body.ack) {
-        res.sendStatus(204);
-        return;
-      }
-
+    promise.then(info => {
       const emptyData = '0000000000000000';
       let downlink = new Object();
-      const data = toDownlinkData(interval);
-      downlink[device] = {
+      const data = toDownlinkData(info.interval);
+      downlink[id] = {
         'downlinkData': (data ? data: emptyData)
       };
 
       res.json(downlink);
-    };
+    }).catch(() => {
+      let downlink = new Object();
+      downlink[id] = {
+        'downlinkData': toDownlinkData('0000000000000000')
+      };
 
-    getDevice(device).then(results => {
-      saveAndReturn(results[0].interval);
-    }).catch(err => {
-      saveAndReturn(3600);
+      res.json(downlink);
     });
     return;
 
@@ -87,37 +142,30 @@ exports.devices = (req, res) => {
     res.sendStatus(405);
     return;
   }
-
-  datastore.get(key, function(err, entity) {
-    let intervalSec = entity.val;
-
-    const emptyData = '0000000000000000';
-    let downlink = new Object();
-    const data = toDownlinkData(intervalSec);
-    downlink[device] = {
-      'downlinkData': (data ? data: emptyData)
-    };
-
-    res.json(downlink);
-  });
 };
 
-exports.interval = (req, res) => {
-  datastore.get(key, function(err, entity) {
-    let intervalSec = entity.val;
+exports.info = (req, res) => {
+  console.log(req.params[0]);
+  console.log(req.body);
 
-    if (req.query.hasOwnProperty('set')) {
-      const v = parseInt(req.query.set);
-      if (!isNaN(v) && toDownlinkData(v)) {
-        intervalSec = v;
-        datastore.upsert({
-          key: key,
-          data: { val: intervalSec }
-        });
-      }
-    }
-    console.log(toDownlinkData(intervalSec));
+  const id = req.params[0].substr(1);
+  if (!id) {
+    res.sendStatus(400);
+    return;
+  }
 
-    res.send(intervalSec.toString());
-  });
+  switch (req.method) {
+  case 'GET':
+    getInfo(id).then(data => res.json(data)).catch(() => res.sendStatus(404));
+    return;
+
+  case 'POST':
+    saveInfo(id, req.body).then(() => res.sendStatus(200))
+      .catch(() => res.sendStatus(500));
+    return;
+
+  default:
+    res.sendStatus(405);
+    return;
+  }
 };
